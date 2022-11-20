359987b5db109b041e1256f3a263417a87d28be4
Jason Zenarosa <jasonzenarosa@gmail.com>
2022-11-20 14:57:49 -0800

    Spinning animation

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="main">
       <LoginForm />
       <div className="petr">
-        <img className="petr-image" src='https://s3.us-west-2.amazonaws.com/secure.notion-static.com/b9a7595c-79f0-4c8c-863b-8b2cec78fcba/petr.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221120%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221120T210405Z&X-Amz-Expires=86400&X-Amz-Signature=5c2b4b63ef00dfc4e29ca7ecbba1c846e44020fb50cf5a15b668290c303163bd&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22petr.png%22&x-id=GetObject' alt='petr'></img>
+        <img className="petr-image spin" src='https://s3.us-west-2.amazonaws.com/secure.notion-static.com/b9a7595c-79f0-4c8c-863b-8b2cec78fcba/petr.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221120%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221120T210405Z&X-Amz-Expires=86400&X-Amz-Signature=5c2b4b63ef00dfc4e29ca7ecbba1c846e44020fb50cf5a15b668290c303163bd&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22petr.png%22&x-id=GetObject' alt='petr'></img>
       </div>
     </div>
   );
